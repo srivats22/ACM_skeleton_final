f20d2927aa7621d51aab0dae3d2b931e5eb3d42a
srivats22 <srivats.venkataraman@gmail.com>
2019-01-04 14:38:13 -0800

    Created an explore page

--- app/src/main/java/com/example/android/acmmobiletest/Membership.java
+++ app/src/main/java/com/example/android/acmmobiletest/Membership.java
@@ -1,5 +1,7 @@
 package com.example.android.acmmobiletest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +15,21 @@ public class Membership extends AppCompatActivity {
     }
 
     public void Gpay(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(
+                "https://pay.google.com/payments/u/0/home#"));
+        startActivity(intent);
     }
 
     public void paypal(View view) {
+        Intent intent = new Intent((Intent.ACTION_VIEW));
+        intent.setData(Uri.parse("https://www.paypal.com/us/webapps/mpp/send-money-online"));
+        startActivity(intent);
     }
 
     public void venmo(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://venmo.com/"));
+        startActivity(intent);
     }
 }
